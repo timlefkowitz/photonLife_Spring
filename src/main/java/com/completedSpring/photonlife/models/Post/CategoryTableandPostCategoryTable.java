286f5564be63af2c8b7b96569fa1b195f69bc373
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-06-30 20:01:20 -0500

    Feat: CategoryTableandPostTable update

--- src/main/java/com/completedSpring/photonlife/models/Post/CategoryTableandPostCategoryTable.java
+++ src/main/java/com/completedSpring/photonlife/models/Post/CategoryTableandPostCategoryTable.java
@@ -12,10 +12,7 @@ public class CategoryTableandPostCategoryTable {
     private long id;
 
     @Column(nullable = false, length = 100)
-    private String AuthorId;
-
-    @Column(nullable = false, length = 100)
-    private String ParentId;
+    private Long ParentId;
 
     @Column(nullable = false, length = 100)
     private String Title;
@@ -28,4 +25,66 @@ public class CategoryTableandPostCategoryTable {
 
     @Column(nullable = false, length = 50)
     private String Content;
+
+
+    //[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
+    //[][][][][][][][][][]    Constructors
+
+
+    public CategoryTableandPostCategoryTable(long id, Long parentId, String title, String metaTitle, String slug, String content) {
+        this.id = id;
+        ParentId = parentId;
+        Title = title;
+        MetaTitle = metaTitle;
+        Slug = slug;
+        Content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Long getParentId() {
+        return ParentId;
+    }
+
+    public void setParentId(Long parentId) {
+        ParentId = parentId;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getMetaTitle() {
+        return MetaTitle;
+    }
+
+    public void setMetaTitle(String metaTitle) {
+        MetaTitle = metaTitle;
+    }
+
+    public String getSlug() {
+        return Slug;
+    }
+
+    public void setSlug(String slug) {
+        Slug = slug;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
 }
