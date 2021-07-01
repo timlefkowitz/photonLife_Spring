bef7ca6d478e7715bfcc875893005ad2c13dc707
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-06-30 20:03:36 -0500

    Feat: CategoryTableandPostTable constructors getters and setters

--- src/main/java/com/completedSpring/photonlife/models/Post/PostCategoryTable.java
+++ src/main/java/com/completedSpring/photonlife/models/Post/PostCategoryTable.java
@@ -9,4 +9,32 @@ public class PostCategoryTable {
 
     @Column(nullable = false, length = 100)
     private String ParentId;
+
+    //// [][][][][]][ [][][][][]][ [][][][][]][ [][][][][]][ [][][][][]][ [][][][][]][ [][][][][]][
+    //
+    //    Constructors || programming is fun, It is fun to look at java
+    //
+    // [][][][][]][ [][][][][]][ [][][][][]][ [][][][][]][ [][][][][]][ [][][][][]][ [][][][][]][ [][][][][]][
+
+
+    public PostCategoryTable(Long postId, String parentId) {
+        PostId = postId;
+        ParentId = parentId;
+    }
+
+    public Long getPostId() {
+        return PostId;
+    }
+
+    public void setPostId(Long postId) {
+        PostId = postId;
+    }
+
+    public String getParentId() {
+        return ParentId;
+    }
+
+    public void setParentId(String parentId) {
+        ParentId = parentId;
+    }
 }
