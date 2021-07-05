8f928687f2c1a4aa6cf090200128d626572cd59d
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-05 13:36:34 -0500

    Feat: Fizzbuzz practice

--- src/main/java/com/completedSpring/photonlife/CodingQuestions.java
+++ src/main/java/com/completedSpring/photonlife/CodingQuestions.java
@@ -121,6 +121,23 @@ public class CodingQuestions {
         }
     }
 
+    private static void fizzbuzz8(int num)
+    {
+        for (int i = 1; i <= num ; i++)
+        {
+            if(((i % 5) == 0) && (( i % 7 ) == 0))
+               System.out.println("FizzBuzz");
+            else if((i % 5) == 0)
+                System.out.println("Fizz");
+            else if ((i % 7) == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+                System.out.println(i);
+        }
+    }
+
 
 
 
