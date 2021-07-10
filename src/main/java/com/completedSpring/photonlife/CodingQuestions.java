bee4b9a9a0814034d8ba0fe41b080b0fd2d4268d
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-09 20:50:43 -0500

    fibonacci14

--- src/main/java/com/completedSpring/photonlife/CodingQuestions.java
+++ src/main/java/com/completedSpring/photonlife/CodingQuestions.java
@@ -505,6 +505,29 @@ public class CodingQuestions {
                                                 counter = counter + 1;
                                     }
                         }
+                        
+                        
+                        static void Fibonacci14(int num)
+                        {
+                                    int num1 = 0; int num2;
+                                    int counter = 0;
+                                    
+                                    //Iterate till counter is num
+                                    while (counter < num){
+                                                //print the number
+                                                System.out.print(num1 + " ");
+                                                
+                                                //SWAP 
+                                                int num3 = num2 + num1;
+                                                num1 = num2;
+                                                num2 = num3;
+                                                counter = counter + 1;
+                                    }
+                                    
+                        }
+                        
+                        
+                                 
 
 
 
