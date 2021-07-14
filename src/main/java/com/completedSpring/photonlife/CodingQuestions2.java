61cb0deef6f89efb646fd2a7cbd80316f2e11420
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-13 21:27:47 -0500

    Anagram3

--- src/main/java/com/completedSpring/photonlife/CodingQuestions2.java
+++ src/main/java/com/completedSpring/photonlife/CodingQuestions2.java
@@ -32,46 +32,67 @@
     return true;
   }
 
-// Driver Code
-public static void main(String[] args)
-{
-   char str1[] = {'t','e','s','t'} ;
-   char str2[] = {'e','t','t','s'} ;
-  
-  
-  //function call
-  if(areAnagram(str1,str2))
-    System.out.print("The two strings are" + " anagram of each other");
-  else
-    System.out.print("The two strings are not" + " anagram of each other");
-}
+      // Driver Code
+      public static void main(String[] args)
+      {
+         char str1[] = {'t','e','s','t'} ;
+         char str2[] = {'e','t','t','s'} ;
+
+
+        //function call
+        if(areAnagram(str1,str2))
+          System.out.print("The two strings are" + " anagram of each other");
+        else
+          System.out.print("The two strings are not" + " anagram of each other");
+      }
 
 
 
 // 2
 
-static boolean areAnagram(char[] str1, char[] str2)
-{
- // Get lenghts of both strings 
-      int String1 = str1.length;
-      int String2 = str2.length;
-      
-      //If length of both strings is not the same, then they cannot be anagram
-      
-      if(String1 != String2)
-            return false;
-      
-      // Sort both Strings
-      Arrays.sort(str1);
-      Arrays.sort(str2);
-      
-      // Compare sorted Strings
-      for (int i = 0 ; i < n1; i++ )
-            if(str1[i] != str2[i])
+      static boolean areAnagram(char[] str1, char[] str2)
+            {
+             // Get lenghts of both strings 
+                  int String1 = str1.length;
+                  int String2 = str2.length;
+
+                  //If length of both strings is not the same, then they cannot be anagram
+
+                  if(String1 != String2)
+                        return false;
+
+                  // Sort both Strings
+                  Arrays.sort(str1);
+                  Arrays.sort(str2);
+
+                  // Compare sorted Strings
+                  for (int i = 0 ; i < n1; i++ )
+                        if(str1[i] != str2[i])
+                              return false;
+
+                  return true;
+            }
+
+
+      static boolean areAnagram(char[] str1, char[] str2)
+      {
+       // Get lengths of both strings
+            int String1 = str1.length;
+            int String1 = str1.length;
+            
+            if(String1 != String2)
                   return false;
-      
-      return true;
-}
+            
+            Arrays.sort(str1);
+             Arrays.sort(str2);
+            
+            for(int i = 0; i< n1; i++)
+                  if(str1[i] != str2[i])
+                        return false
+                        
+                       return true;
+            
+      }
 
 }
 
